095fa011381acd2dff8d7480b39d5535a84c171a
aavasilev <aavasilev@saber3d.ru>
2021-03-22 13:13:51 +0300

    Fix View matrix calculation

--- DX11Tutorial01/DX11Tutorial01/Renderer.cpp
+++ DX11Tutorial01/DX11Tutorial01/Renderer.cpp
@@ -203,8 +203,7 @@ bool Renderer::Update()
 	static const float farPlane = 100.0f;
 	static const float fov = (float)M_PI * 2.0 / 3.0;
 
-	XMMATRIX view = XMMatrixInverse(NULL, XMMatrixTranslation(0, 0, -10.0f));
-	view = XMMatrixRotationAxis({ 0,1,0 }, m_lon) * XMMatrixRotationAxis({ 1,0,0 }, m_lat) * view;
+	XMMATRIX view = XMMatrixInverse(NULL, XMMatrixTranslation(0, 0, -10.0f) * XMMatrixRotationAxis({ 1,0,0 }, m_lat) * XMMatrixRotationAxis({ 0,1,0 }, m_lon));
 
 	float width = nearPlane / tanf(fov / 2.0);
 	float height = ((float)m_height / m_width) * width;
@@ -240,9 +239,9 @@ bool Renderer::Render()
 
 void Renderer::MouseMove(int dx, int dy)
 {
-	m_lon += -(float)dx / m_width * 5.0f;
+	m_lon += (float)dx / m_width * 5.0f;
 
-	m_lat += -(float)dy / m_height * 5.0f;
+	m_lat += (float)dy / m_height * 5.0f;
 	if (m_lat <= -(float)M_PI / 2)
 	{
 		m_lat = -(float)M_PI / 2;
